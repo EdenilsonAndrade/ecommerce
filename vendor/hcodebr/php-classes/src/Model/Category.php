816e850816fc6bcf47e0d02acd7b4aebc2f36536
EdenilsonAndrade <edenilsonbass@hotmail.com>
2020-05-19 14:44:48 -0300

    Criado a paginação ao selecionar por uma categoria, aula 112

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -149,6 +149,34 @@ class Category extends Model {
 		]);
 
 	}
+	// metodo para paginação
+	public function getProductsPage($page = 1, $itemsPerPage = 8)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS * 
+			FROM tb_products a 
+			INNER JOIN tb_productscategories b ON (a.idproduct = b.idproduct)
+			INNER JOIN tb_categories c ON (c.idcategory = b.idcategory)
+			WHERE c.idcategory = :idcategory
+			LIMIT $start, $itemsPerPage;
+		", [
+			":idcategory"=>$this->getidcategory()
+		]);
+
+		$resultsTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal");
+
+		return [
+			"data"=>Product::checkList($results), 
+			"total"=>(int)$resultsTotal[0]["nrtotal"],
+			"pages"=>ceil($resultsTotal[0]["nrtotal"] / $itemsPerPage) //a função ceil aredonda para cima, se utilizar o exemplo de 10 produtos por paginas e eu conter 11 ira gerar um número de paginas de 1,1, porém precisar gerar duas páginas, com o ceil irá fazer isso
+		];
+
+	}
 
 }
 
